4a1b0fdadba3f409559f184b26e9d24ce083ac19
igordamicost <82038310+igordamicost@users.noreply.github.com>
2021-08-23 19:18:44 -0300

    Configurando app list

--- client-room/src/app/app.module.ts
+++ client-room/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateRoomComponent } from './create-room/create-room.component';
+import { RoomListComponent } from './room-list/room-list.component';
+import { UpdateRoomComponent } from './update-room/update-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRoomComponent,
+    RoomListComponent,
+    UpdateRoomComponent
   ],
   imports: [
     BrowserModule,
